20486959d8026478d9f8f9ed767a6cededf7d699
dipikasivakumar <dipikasivakumar97@gmail.com>
2017-02-20 22:40:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main text');
 element.innerHTML='nw value';
 
 //move the img
-var img= document.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
